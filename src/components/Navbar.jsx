27fd2a2073ca00b82cbaad8db5a8f40d39b14c60
Abu Nasim <info.abunasim@gmail.com>
2026-04-23 08:03:44 +0600

    fix: update UNDP logo URL to 1280px version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -220,7 +220,7 @@ const Navbar = () => {
               className="flex items-center gap-3 group"
             >
               <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/UNDP_logo.svg/1011px-UNDP_logo.svg.png"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/UNDP_logo.svg/1280px-UNDP_logo.svg.png"
                 alt="UNDP Logo"
                 className="h-8 md:h-10 w-auto transition-transform duration-300 group-hover:scale-110"
               />
